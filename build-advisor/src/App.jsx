6e1c96538e21329de6e309d21c898ab92af8fc0e
Kyle Murphy <murphy.kyle.r@gmail.com>
2026-01-23 10:38:03 -0500

    Changed to have tutorial page

--- build-advisor/src/App.jsx
+++ build-advisor/src/App.jsx
@@ -1,5 +1,14 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import BuildAdvisor from "./BuildAdvisor";
+import { BuildJsonTutorial } from "./pages/BuildJsonTutorial";
 
 export default function App() {
-  return <BuildAdvisor />;
+  return (
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
+      <Routes>
+      <Route path="/" element={<BuildAdvisor />} />
+      <Route path="/tutorial" element={<BuildJsonTutorial />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
